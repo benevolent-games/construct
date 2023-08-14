f2ca89c454b9eb44cb6da363a0d287fb9324aaf9
Chase Moskal <chasemoskal@gmail.com>
2023-08-14 11:40:22 -0700

    sketch: glb data printout

--- s/elements/ed-catalog/elements.ts
+++ s/elements/ed-catalog/elements.ts
@@ -1,12 +1,23 @@
 
-import {html} from "lit"
+import {TemplateResult, html} from "lit"
 import {QuickElement} from "@benev/frog"
 import {Context} from "../../context/context.js"
 import {human_bytes} from "../../tools/human_bytes.js"
 
 export const EdCatalog = (context: Context) => class extends QuickElement {
 
+	#list(stuff: TemplateResult[]) {
+		return html`
+			<ol>
+				${stuff.map(s => html`
+					<li>${s}</li>
+				`)}
+			</ol>
+		`
+	}
+
 	render() {
+		const name = (n: {name: string}) => html`${n.name}`
 		return html`
 			<div>catalog</div>
 			<ul>
@@ -14,6 +25,18 @@ export const EdCatalog = (context: Context) => class extends QuickElement {
 					<li>
 						<div>${glb.name}</div>
 						<div>${human_bytes(glb.size)}</div>
+
+						<h2>transform nodes</h2>
+						${this.#list(glb.container.transformNodes.map(name))}
+
+						<h2>meshes</h2>
+						${this.#list(glb.container.meshes.map(name))}
+
+						<h2>materials</h2>
+						${this.#list(glb.container.materials.map(name))}
+
+						<h2>textures</h2>
+						${this.#list(glb.container.textures.map(name))}
 					</li>
 				`)}
 			</ul>
