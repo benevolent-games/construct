86f96c0320bc781826a384822678e1520cd964fe
Paul_Aroo <timmypaul2006@gmail.com>
2023-08-16 08:34:19 +0100

    implement: select and deselect in world

--- s/context/controllers/world/world.ts
+++ s/context/controllers/world/world.ts
@@ -1,14 +1,18 @@
 
-import {NubContext} from "@benev/nubs"
+import "@babylonjs/core/Culling/ray.js"
+import "@babylonjs/core/Rendering/edgesRenderer.js"
+
 import {Scene} from "@babylonjs/core/scene.js"
 import {spawn_light} from "@benev/toolbox/x/demo/spawn_light.js"
+import {NubContext, NubDetail, NubEffectEvent} from "@benev/nubs"
 import {TransformNode} from "@babylonjs/core/Meshes/transformNode.js"
 import {make_fly_camera} from "@benev/toolbox/x/babylon/flycam/make_fly_camera.js"
 import {integrate_nubs_to_control_fly_camera} from "@benev/toolbox/x/babylon/flycam/integrate_nubs_to_control_fly_camera.js"
 
 import {Graph} from "../graph/graph.js"
+import {make_box} from "./parts/make-box.js"
 import {Id, Unit} from "../graph/parts/types.js"
-import {make_box} from "../../../tools/make-box.js"
+import {InstancedMesh} from "@babylonjs/core/Meshes/instancedMesh.js"
 
 export class World {
 	#scene: Scene
@@ -23,9 +27,15 @@ export class World {
 		this.#scene = scene
 		graph.on.added(this.#add)
 		graph.on.removed(this.#remove)
+		graph.on.selected(this.#select)
+		graph.on.deselected(this.#deselect)
+
+		NubEffectEvent.target(window)
+			.listen(this.#manage_effect_events)
 	}
 
 	#add = ([id, item]: [Id, Unit]) => {
+		item.node.id = id
 		const instance = item.node.instantiateHierarchy()!
 		this.#instances.set(id, instance)
 		this.#scene.addTransformNode(instance)
@@ -35,6 +45,42 @@ export class World {
 		const instance = this.#instances.get(id)!
 		instance.dispose()
 		this.#instances.delete(id)
+	}
+
+	#select = (id: Id) => {
+		const instance = this.#instances.get(id)!
+		const mesh = instance.getChildMeshes(true)[0]
+		mesh.enableEdgesRendering()
+		mesh.edgesWidth = 8
+	}
+
+	#deselect = (id: Id) => {
+		const instance = this.#instances.get(id)!
+		const mesh = instance.getChildMeshes(true)[0]
+		mesh.disableEdgesRendering()
+	}
+
+	#manage_effect_events = ({detail}: NubEffectEvent) => {
+		switch (detail.effect) {
+			case "select": {
+				const pressed = (detail as NubDetail.Key).pressed
+				if (pressed) {
+					const pick = this.#scene.pick(
+						this.#scene.pointerX, this.#scene.pointerY
+					)
+					if (pick?.hit) {
+						const pickedMesh = pick.pickedMesh as unknown as InstancedMesh
+						const id = pickedMesh.parent!.id
+						if(this.#graph.selected(id)) {
+							this.#graph.deselect(id)
+						} else {
+							this.#graph.select(id)
+						}
+					}
+				}
+				break
+			}
+		}
 	}
 
 	start_world({
--- s/main.ts
+++ s/main.ts
@@ -7,8 +7,8 @@ import {BenevTheater} from "@benev/toolbox/x/babylon/theater/element.js"
 
 import {Context} from "./context/context.js"
 import {drag_and_drop} from "./tools/drag_and_drop.js"
-import {editor_schema} from "./tools/editor-schema.js"
 import {prepare_elements} from "./elements/prepare_elements.js"
+import {editor_schema} from "./context/controllers/world/parts/editor-schema.js"
 
 const theater = document.querySelector<BenevTheater>("benev-theater")!
 await (theater.updateComplete)
@@ -22,15 +22,13 @@ drag_and_drop(document.documentElement, async list => {
 		context.catalog.add_file(file)
 })
 
-const nubContext = theater.nubContext
-if (nubContext) {
-	nubContext.modes_set.assign(["selection", "transform", "fly"])
-	nubContext!.schema = editor_schema
-	context.world.start_world({
-		nubContext,
-		renderLoop: theater.babylon.renderLoop
-	})
-}
+const nubContext = theater.nubContext!
+nubContext.modes_set.assign(["selection", "transform", "fly"])
+nubContext.schema = editor_schema
+context.world.start_world({
+	nubContext,
+	renderLoop: theater.babylon.renderLoop
+})
 
 theater.babylon.start()
 
