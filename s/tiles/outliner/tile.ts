255ae6f4fdc9b5a5c885e8626ba1999c1b5f322e
Chase Moskal <chasemoskal@gmail.com>
2023-10-22 15:21:52 -0700

    add: outliner item delete

--- s/tiles/outliner/tile.ts
+++ s/tiles/outliner/tile.ts
@@ -4,13 +4,16 @@ import {generateId} from "@benev/toolbox/x/utils/generate-id.js"
 
 import {styles} from "./styles.js"
 import {tile} from "../tile_parts.js"
+import { EzMap } from "../../tools/ezmap.js"
 import {obsidian} from "../../context/context.js"
-import {Item} from "../../context/domains/outline/types.js"
+import {Id, Item} from "../../context/domains/outline/types.js"
 import {sprite_layers} from "../../sprites/groups/feather/layers.js"
 import { sprite_plus } from "../../sprites/groups/feather/plus.js"
 import { sprite_tabler_folder } from "../../sprites/groups/tabler/folder.js"
 import { sprite_tabler_eye } from "../../sprites/groups/tabler/eye.js"
 import { make_outline_tools } from "../../context/domains/outline/tools.js"
+import { sprite_tabler_folder_filled } from "../../sprites/groups/tabler/folder-filled.js"
+import { sprite_x } from "../../sprites/groups/feather/x.js"
 
 export const OutlinerTile = tile({
 	label: "outliner",
@@ -19,7 +22,14 @@ export const OutlinerTile = tile({
 		const outline = use.watch(() => use.context.state.outline)
 		const {actions} = use.context
 
-		const tools = make_outline_tools(outline)
+		const localFolderSettings = use.prepare(() => new EzMap<Id, {opened: boolean}>())
+		// const tools = make_outline_tools(outline)
+
+		function get_local_folder_settings(id: Id) {
+			return localFolderSettings.guarantee(id, () => ({
+				opened: true,
+			}))
+		}
 
 		function click_to_create_new_folder(parent: Item.Folder) {
 			return () => {
@@ -46,35 +56,61 @@ export const OutlinerTile = tile({
 		}
 
 		function render_flat(item: Item.Whatever, parents: Item.Folder[]): TemplateResult {
+			function render_line_item(content: TemplateResult) {
+				const parent = parents.at(-1)
+				const delete_this_item = parent
+					? () => actions.delete_item({
+						changes: [{item, folderId: parent.id}]
+					})
+					: undefined
+
+				return html`
+					<li data-kind="${item.kind}">
+						${render_gutters(parents)}
+						${content}
+						${delete_this_item
+							? html`
+								<button class=delete @click=${delete_this_item}>
+									${sprite_x}
+								</button>
+							`
+							: undefined}
+					</li>
+				`
+			}
+
 			switch (item.kind) {
-				case "instance": return html`
-					<li>
-						${render_gutters(parents)}
+				case "instance":
+					return render_line_item(html`
 						<span class=name>${item.name}</span>
-					</li>
-				`
-				case "light": return html`
-					<li>
-						${render_gutters(parents)}
+					`)
+				case "light":
+					return render_line_item(html`
 						<span class=name>${item.name}</span>
-					</li>
-				`
-				case "folder": return html`
-					<li>
-						${render_gutters(parents)}
-						<button class=icon>
-							${sprite_tabler_folder}
-						</button>
-						<span class=name>${item.name}</span>
-						<button class=newfolder @click=${click_to_create_new_folder(item)}>
-							${sprite_plus}
-						</button>
-						<button class=visibility>
-							${sprite_tabler_eye}
-						</button>
-					</li>
-					${item.children.map(child => render_flat(child, [...parents, item]))}
-				`
+					`)
+				case "folder":
+					const settings = get_local_folder_settings(item.id)
+					const toggle_opened = () => {
+						settings.opened = !settings.opened
+						use.rerender()
+					}
+					return html`
+						${render_line_item(html`
+							<button class=icon @click=${toggle_opened}>
+								${settings.opened ?sprite_tabler_folder_filled :sprite_tabler_folder}
+							</button>
+							<span class=name>${item.name}</span>
+							<button class=newfolder @click=${click_to_create_new_folder(item)}>
+								${sprite_plus}
+							</button>
+							<button class=visibility>
+								${sprite_tabler_eye}
+							</button>
+						`)}
+						${settings.opened
+							? item.children.map(child => render_flat(child, [...parents, item]))
+							: undefined}
+					`
 			}
 		}
 
