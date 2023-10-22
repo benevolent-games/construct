38d2dc280ecb757f972e97b8dc09b2994c479568
Chase Moskal <chasemoskal@gmail.com>
2023-10-22 14:36:48 -0700

    improve: outliner rendering and styles

--- s/tiles/outliner/styles.ts
+++ s/tiles/outliner/styles.ts
@@ -10,5 +10,51 @@ ${standard_tile_styles}
 	overflow-y: auto;
 }
 
+ol {
+	list-style: none;
+}
+
+li {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	width: 100%;
+	gap: 0.5em;
+	padding: 0 1em;
+
+	&:nth-child(odd) {
+		background: #8881;
+	}
+
+	> * {
+		flex: 0;
+	}
+
+	> .gutter {
+		width: 0.5em;
+		height: 1.2em;
+		margin-left: 0.5em;
+		border-left: 1px solid #fff2;
+	}
+
+	> button {
+		opacity: 0.5;
+		&:hover { opacity: 0.9; }
+		&:active { opacity: 1; }
+
+		> svg {
+			display: block;
+			width: 1em;
+			height: 1em;
+		}
+	}
+
+	> .name {
+		flex: 1 1 auto;
+		word-break: break-all;
+		line-height: 1;
+	}
+}
+
 `
 
--- s/tiles/outliner/tile.ts
+++ s/tiles/outliner/tile.ts
@@ -6,63 +6,81 @@ import {styles} from "./styles.js"
 import {tile} from "../tile_parts.js"
 import {obsidian} from "../../context/context.js"
 import {Item} from "../../context/domains/outline/types.js"
-import {sprite_folder} from "../../sprites/groups/feather/folder.js"
+import {sprite_layers} from "../../sprites/groups/feather/layers.js"
+import { sprite_plus } from "../../sprites/groups/feather/plus.js"
+import { sprite_tabler_folder } from "../../sprites/groups/tabler/folder.js"
+import { sprite_tabler_eye } from "../../sprites/groups/tabler/eye.js"
+import { make_outline_tools } from "../../context/domains/outline/tools.js"
 
 export const OutlinerTile = tile({
 	label: "outliner",
-	icon: sprite_folder,
+	icon: sprite_layers,
 	view: obsidian({name: "outliner", styles}, use => () => {
 		const outline = use.watch(() => use.context.state.outline)
 		const {actions} = use.context
 
-		function render_item(item: Item.Whatever): TemplateResult {
+		const tools = make_outline_tools(outline)
+
+		function click_to_create_new_folder(parent: Item.Folder) {
+			return () => {
+				const new_id = generateId()
+				actions.add_item({
+					changes: [{
+						folderId: parent.id,
+						item: {
+							kind: "folder",
+							id: new_id,
+							name: `new folder ${new_id.slice(0, 5)}`,
+							selected: false,
+							children: [],
+						},
+					}],
+				})
+			}
+		}
+
+		function render_gutters(parents: Item.Folder[]) {
+			return parents.map(() => html`
+				<span class=gutter></span>
+			`)
+		}
+
+		function render_flat(item: Item.Whatever, parents: Item.Folder[]): TemplateResult {
 			switch (item.kind) {
-
-				case "folder": return html`
-					<li>
-						<button class=icon>[folder]</button>
-						<span class=name>${item.name}</span>
-						<button class=newfolder @click=${() => {
-							const new_id = generateId()
-							actions.add_item({
-								changes: [{
-									folderId: item.id,
-									item: {
-										kind: "folder",
-										id: new_id,
-										name: `new folder ${new_id.slice(0, 5)}`,
-										selected: false,
-										children: [],
-									},
-								}],
-							})
-						}}>+</button>
-						<ol>
-							${item.children.map(item => render_item(item))}
-						</ol>
-					</li>
-				`
-
 				case "instance": return html`
 					<li>
+						${render_gutters(parents)}
 						<span class=name>${item.name}</span>
 					</li>
 				`
-
 				case "light": return html`
 					<li>
+						${render_gutters(parents)}
 						<span class=name>${item.name}</span>
 					</li>
 				`
-
-				default:
-					throw new Error(`unknown item kind "${(item as any).kind}"`)
+				case "folder": return html`
+					<li>
+						${render_gutters(parents)}
+						<button class=icon>
+							${sprite_tabler_folder}
+						</button>
+						<span class=name>${item.name}</span>
+						<button class=newfolder @click=${click_to_create_new_folder(item)}>
+							${sprite_plus}
+						</button>
+						<button class=visibility>
+							${sprite_tabler_eye}
+						</button>
+					</li>
+					${item.children.map(child => render_flat(child, [...parents, item]))}
+				`
 			}
 		}
 
 		return html`
 			<ol>
-				${render_item(outline)}
+				${render_flat(outline, [])}
 			</ol>
 		`
 	}),
