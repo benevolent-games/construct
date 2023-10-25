3c5674011685e05eb94638d2863095d814498c04
Chase Moskal <chasemoskal@gmail.com>
2023-10-25 02:23:22 -0700

    delete: old icons

--- s/icons/akar/arrow-down.svg.ts
+++
@@ -1,3 +0,0 @@
-import {svg} from "lit"
-
-export default svg`<svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="currentColor" stroke-width="2" class="ai ai-TriangleDownFill"><path d="M6 8a1 1 0 0 0-.8 1.6l6 8a1 1 0 0 0 1.6 0l6-8A1 1 0 0 0 18 8H6z"/></svg>`
