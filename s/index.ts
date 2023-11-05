ae8293842a9f1fee4da7a46edb3d47d2dc113c0f
Chase Moskal <chasemoskal@gmail.com>
2023-11-04 23:27:13 -0700

    export: panel parts

--- s/index.ts
+++ s/index.ts
@@ -4,5 +4,9 @@ export * from "./panels/panels.js"
 export * from "./context/context.js"
 export * from "./elements/construct-editor/element.js"
 
+export * from "./panels/panel_parts.js"
+export * from "./panels/placeholder_panel_styles.js"
+export * from "./panels/standard_panel_styles.js"
+
 export * from "./tools/quick_hash.js"
 
