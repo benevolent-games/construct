3c0674b757973e7a6f4f75df361e77495ddda385
Chase Moskal <chasemoskal@gmail.com>
2023-11-13 03:16:26 -0800

    sketch: new item types with scenes

--- s/context/domains/outline/types.ts
+++ s/context/domains/outline/types.ts
@@ -1,45 +1,99 @@
 
 import {Spatial} from "./spatial.js"
 import {Id} from "../../../tools/fresh_id.js"
+import {V3} from "@benev/toolbox/x/utils/v3.js"
 import {PropAddress} from "../../controllers/world/warehouse/parts/types.js"
 
 export namespace Item {
-	export type Kind = "folder" | "instance" | "light"
+	export type Kind = (
+		| "folder"
+		| "instance"
+		| "light"
+		| "placement"
+	)
+
+	export type FolderSubtype = (
+		| "root"
+		| "subroot"
+		| "scene"
+		| "subscene"
+		| "arrangement"
+	)
 
 	export interface Base {
 		kind: Kind
 		id: Id
 		name: string
 		selected: boolean
-		visible: boolean
 	}
 
 	export interface Folder extends Base {
 		kind: "folder"
-		name: string
-		children: Whatever[]
+		subtype: FolderSubtype
+		children: Anything[]
 	}
 
-	export interface Instance extends Base {
+	export interface Manifestation extends Base {
+		visible: boolean
+		spatial: Spatial
+	}
+
+	export type RootChild = SubrootFolder | Scene
+	export type ArrangementChild = SubsceneFolder | Instance | Light
+	export type SceneChild = ArrangementFolder | ArrangementChild
+
+	//////
+	//////
+	//////
+
+	export interface Root extends Folder {
+		folder: "root"
+		children: RootChild[]
+		selected: false
+	}
+
+	export interface SubrootFolder extends Folder {
+		subtype: "subroot"
+		children: RootChild[]
+	}
+
+	export interface Scene extends Folder {
+		subtype: "scene"
+		children: SceneChild[]
+	}
+
+	export interface SubsceneFolder extends Folder {
+		subtype: "subscene"
+		children: SceneChild[]
+	}
+
+	export interface ArrangementFolder extends Folder {
+		subtype: "arrangement"
+		children: ArrangementChild[]
+		pivot: V3
+	}
+
+	export interface Instance extends Manifestation {
 		kind: "instance"
-		name: string
 		address: PropAddress
-		spatial: Spatial
 	}
 
-	export interface Light extends Base {
+	export interface Light extends Manifestation {
 		kind: "light"
-		name: string
-		spatial: Spatial
 	}
 
-	export type Whatever = Folder | Instance | Light
+	export interface Placement extends Manifestation {
+		kind: "placement"
+	}
 
-	////////
-	////////
+	export type Anything = Root | Scene | Folder | Instance | Light | Placement
+
+	//////
+	//////
+	//////
 
 	export type Report = {
-		item: Item.Whatever
+		item: Item.Anything
 		parent: Item.Folder
 		parents: Item.Folder[]
 	}
