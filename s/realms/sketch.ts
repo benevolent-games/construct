8416df9bae5c1deb40edaf922a8f089c53ff88d5
Chase Moskal <chasemoskal@gmail.com>
2023-12-04 20:29:14 -0800

    sketch: refactor core nas syntax

--- s/realms/sketch.ts
+++ s/realms/sketch.ts
@@ -9,16 +9,10 @@ export namespace Core {
 	export type AspectSchema = {[kind: Kind]: Aspect}
 	export type AsAspectSchema<AS extends AspectSchema> = AS
 
-	export type AspectReport<K extends keyof any, A> = {
-		id: Id
-		kind: K
-		aspect: A
-	}
-
-	export type NodeReport<Aspects extends Partial<AspectSchema> = Partial<AspectSchema>> = {
-		id: Id,
-		aspects: Aspects,
-	}
+	export type AspectReport<Kind extends keyof any, Aspect> = [
+		Aspect,
+		Kind,
+	]
 
 	export abstract class System {
 		abstract name: string
@@ -37,19 +31,19 @@ export namespace Core {
 		#nodes = new Map<Id, Set<Id>>()
 		#aspects = new Map<Id, [Kind, Aspect]>()
 
-		node(id: Id) {
+		node(id: Id): Partial<AS> {
 			const set = this.#nodes.get(id)
 			if (!set)
 				throw new Error(`node not found "${id}"`)
-			return {id, aspects: this.#assemble_aspects(...set)} as NodeReport
+			return this.#assemble_aspects(...set)
 		}
 
-		aspect<Kind extends keyof AS = keyof AS>(id: Id) {
+		aspect<Kind extends keyof AS = keyof AS>(id: Id): AspectReport<Kind, AS[Kind]> {
 			const result = this.#aspects.get(id)
 			if (!result)
 				throw new Error(`aspect not found "${id}"`)
 			const [kind, aspect] = result as [Kind, AS[Kind]]
-			return {id, kind, aspect} as AspectReport<Kind, AS[Kind]>
+			return [aspect, kind]
 		}
 
 		create_node(...aspectIds: Id[]) {
@@ -58,20 +52,20 @@ export namespace Core {
 			const id = freshId()
 			const set = new Set<Id>(aspectIds)
 			this.#nodes.set(id, set)
-			return [id, set]
+			return id
 		}
 
 		create_aspect<Kind extends keyof AS>(kind: Kind, aspect: AS[Kind]) {
 			const id = freshId()
-			this.#aspects.set(id, aspect)
-			return {id, kind, aspect} as AspectReport<Kind, AS[Kind]>
+			this.#aspects.set(id, [kind as any, aspect])
+			return id
 		}
 
 		#assemble_aspects(...ids: Id[]) {
 			const aspects: Partial<AS> = {}
 
 			for (const id of ids) {
-				const {kind, aspect} = this.aspect(id)
+				const [aspect, kind] = this.aspect(id)
 				aspects[kind] = aspect
 			}
 
@@ -79,15 +73,15 @@ export namespace Core {
 		}
 
 		*select<Kind extends keyof AS>(...kinds: Kind[]) {
-			for (const [id, aspectIds] of this.#nodes) {
+			for (const [nodeId, aspectIds] of this.#nodes) {
 				const aspects = this.#assemble_aspects(...aspectIds)
 				const node_matches_selector = kinds.every(kind => kind in aspects)
 
 				if (node_matches_selector)
-					yield {
-						id,
+					yield [
 						aspects,
-					} as NodeReport<{[K in Kind]: AS[K]} & Partial<AS>>
+						nodeId,
+					] as [{[K in Kind]: AS[K]} & Partial<AS>, Id]
 			}
 		}
 
@@ -124,9 +118,10 @@ type MyAspects = Core.AsAspectSchema<{
 
 const safe = new Core.Safe<MyAspects>()
 
-const lol = safe.aspect<"position">("")
+const [position] = safe.aspect<"position">("")
+position
 
-for (const {aspects} of safe.select("position")) {
+for (const [aspects] of safe.select("position")) {
 	aspects.position
 }
 
